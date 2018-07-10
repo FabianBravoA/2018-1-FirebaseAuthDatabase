4e7d2dd335e4cdd9c12f500864602b5679b6f12c
Fabián Alexis Bravo Abarca <fabian.bravo.abarca@gmail.com>
2018-07-10 12:14:52 -0400

    Agregar mensaje en la base de datos

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ window.onload = ()=>{
             loggedIn.style.display = "none";
         }
     });
+
+    //Acá comenzamos a escuchar por nuevos mensajes usando el evento
+    //on child_added
+    firebase.database().ref('messages')
+        .on('child_added', (newMessage)=>{
+            messageContainer.innerHTML += `
+                <p>Nombre : ${newMessage.val().creatorName}</p>
+                <p>${newMessage.val().text}</p>
+            `;
+        });
 };
 
 function register(){
@@ -61,4 +71,21 @@ function loginFacebook(){
             console.log("Error de firebase > "+error.code);
             console.log("Error de firebase, mensaje > "+error.message);
         });
+}
+
+// Firebase Database
+
+// Usaremos una colección para guardar los mensajes, llamada messages
+function sendMessage(){
+    const currentUser = firebase.auth().currentUser;
+    const messageAreaText = messageArea.value;
+
+    //Para tener una nueva llave en la colección messages
+    const newMessageKey = firebase.database().ref().child('messages').push().key;
+
+    firebase.database().ref(`messages/${newMessageKey}`).set({
+        creator : currentUser.uid,
+        creatorName : currentUser.displayName,
+        text : messageAreaText
+    });
 }
